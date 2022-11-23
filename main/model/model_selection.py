a996c3a430d3eb1d6028af67d18783317de8f655
ymnseol <ymnseol@gmail.com>
2022-11-23 06:36:57 +0000

    feat: Add the model using entity masks (R-BERT)

--- main/model/model_selection.py
+++ main/model/model_selection.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 from argparse import Namespace
 from transformers import AutoTokenizer, AutoModel, AutoConfig, AutoModelForSequenceClassification
-from model.models import TransformerModel, TransformerModelUsingMask
+from model.models import TransformerModel, TransformerModelUsingMask, TransformerModelUsingEntity
 
 class Selection():
     """ 
@@ -66,6 +66,24 @@ class Selection():
             
             ## Get final model
             self.model = TransformerModelUsingMask(transformer, self.tokenizer.mask_token_id, config)
+            self.model.config = model_config
+
+        ## R-BERT
+        elif self.config.model_type == 3:
+            ## Model config setting
+            model_config = AutoConfig.from_pretrained(self.config.model_name)
+            model_config.num_labels = 30
+            
+            ## Load transformer & tokenizer
+            transformer = AutoModel.from_pretrained(
+                self.config.model_name,
+                config=model_config,
+                add_pooling_layer=False)
+
+            self.tokenizer = AutoTokenizer.from_pretrained(self.config.model_name)
+            
+            ## Get final model
+            self.model = TransformerModelUsingEntity(transformer, config)
             self.model.config = model_config
             
         ## TODO: 다른 모델을 사용할 경우
