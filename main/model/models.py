b613e3b812bf9c8507a67b7c7ebbb95023129f61
ymnseol <ymnseol@gmail.com>
2022-11-29 12:56:38 +0000

    feat: Add the model class for R-BERT

--- main/model/models.py
+++ main/model/models.py
@@ -96,3 +96,76 @@ class TransformerModelUsingMask(nn.Module):
         out = self.sequence(x_mask)
         
         return out
+
+
+class TransformerModelUsingEntity(nn.Module):
+    """
+    R-BERT
+    """
+    def __init__(self, transformer, config, entity_from):
+        super(TransformerModelUsingEntity, self).__init__()
+        ## Setting
+        self.config = config
+        self.entity_from = entity_from
+        
+        ## Transformer model
+        self.transformer = transformer
+        
+        ## Dimension
+        self.h_dim = self.transformer.config.hidden_size
+
+        ## Layers
+        self.cls_activation = nn.Tanh()
+        self.cls_dropout = nn.Dropout(0.1)
+        self.cls_fc = nn.Linear(self.h_dim, self.h_dim)
+
+        self.entity_activation = nn.Tanh()
+        self.entity_dropout = nn.Dropout(0.1)
+        self.entity_fc = nn.Linear(self.h_dim, self.h_dim)
+
+        self.classifier_dropout = nn.Dropout(0.1)
+        self.classifier_fc = nn.Linear(self.h_dim * 3, self.config.num_labels)
+        
+    def forward(self, *input, input_ids, token_type_ids, attention_mask, sub_entity_mask, obj_entity_mask, labels=None):
+        ## Tranformer output
+        if self.entity_from == 'middle':
+            x = self.transformer(
+                input_ids, #kwargs["input_ids"],
+                token_type_ids = token_type_ids, #kwargs["token_type_ids"],
+                attention_mask = attention_mask, #kwargs["attention_mask"],
+                return_dict = True,
+            ) # (batch size, length of sentences, hidden state dim.)
+
+            h0 = x.last_hidden_state[:, 0, :]
+            h1 = torch.bmm(sub_entity_mask.unsqueeze(1).float(), x.hidden_states[7]).squeeze(1) / (sub_entity_mask != 0).sum(dim=1).unsqueeze(1)
+            h2 = torch.bmm(obj_entity_mask.unsqueeze(1).float(), x.hidden_states[7]).squeeze(1) / (obj_entity_mask != 0).sum(dim=1).unsqueeze(1)
+        elif self.entity_from == 'last':
+            x = self.transformer(
+                input_ids, #kwargs["input_ids"],
+                token_type_ids = token_type_ids, #kwargs["token_type_ids"],
+                attention_mask = attention_mask, #kwargs["attention_mask"],
+                return_dict = True,
+            ).last_hidden_state # (batch size, length of sentences, hidden state dim.)
+
+            h0 = x[:, 0, :]
+            h1 = torch.bmm(sub_entity_mask.unsqueeze(1).float(), x).squeeze(1) / (sub_entity_mask != 0).sum(dim=1).unsqueeze(1)
+            h2 = torch.bmm(obj_entity_mask.unsqueeze(1).float(), x).squeeze(1) / (obj_entity_mask != 0).sum(dim=1).unsqueeze(1)
+
+        h0 = self.cls_activation(h0)
+        h0 = self.cls_dropout(h0)
+        h0 = self.cls_fc(h0)
+
+        h1 = self.entity_activation(h1)
+        h1 = self.entity_dropout(h1)
+        h1 = self.entity_fc(h1)
+
+        h2 = self.entity_activation(h2)
+        h2 = self.entity_dropout(h2)
+        h2 = self.entity_fc(h2)
+
+        h = torch.cat([h0, h1, h2], dim=-1) # (batch size, hidden state dim. * 3)
+
+        h = self.classifier_dropout(h)
+        h = self.classifier_fc(h)
+        
+        return h
