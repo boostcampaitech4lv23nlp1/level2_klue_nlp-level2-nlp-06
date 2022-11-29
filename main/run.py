b5187ff365afedf8a4eecfb40c0461e6535919b4
ymnseol <ymnseol@gmail.com>
2022-11-29 12:59:37 +0000

    chore: Add the option for R-BERT
    Add input_type 3 for using R-BERT model and entity_from for selecting the entity hidden state

--- main/run.py
+++ main/run.py
@@ -5,34 +5,35 @@ class Config:
     def __init__(self):
         ## 1. 모델 학습시킬 때 가장 먼저 바꿔야 하는 것
         self.model_name: str = "klue/roberta-large"
-        self.save_path: str = "../../saved_model/QAtype_large.pt" # 최종 모델을 어디에 저장할지
-        self.result_path: str = "../../dataset/submission_QAtype_large.csv" # 마지막 csv 파일을 어디에 저장할지
-        self.wandb_name: str = "'roberta-large:QA+typed'" # wandb 내에서 작업 이름 설정 (중요)
-        self.wandb_group: str = "QA+typed" # wandb group.
-        self.wandb_note: str = "'no focal'" # wandb note
+        self.save_path: str = "../../saved_model/architecture-rbert-focal-klue-roberta-large-2.pt" # 최종 모델을 어디에 저장할지
+        self.result_path: str = "../../dataset/architecture-rbert-focal-klue-roberta-large-2.csv" # 마지막 csv 파일을 어디에 저장할지
+        self.wandb_name: str = "'architecture-rbert-focal-klue-roberta-large-2'" # wandb 내에서 작업 이름 설정 (중요)
+        self.wandb_group: str = "RBERT" # wandb group.
+        self.wandb_note: str = "'architecture-rbert-klue-roberta-large-2'" # wandb note
         
         ## 2. 데이터 위치 (csv 파일)
         self.train_data_path: str = "../../dataset/train/train_80.csv"
-        self.val_data_path: str = "../../dataset/valid/valid_20.csv"
+        self.val_data_path: str = "../../dataset/train/valid_20.csv"
         self.test_data_path: str = "../../dataset/test/test_data.csv"
 
         ## 3. 학습 설정
         '''
         train_type = {0: "base-model", 1: "rescent"}
-        model_type = {0: "base-model", 1: "Masked_QA"}
-        input_type = {0: "base-input", 1: "typed_punct_entity", 2: "Masked_QA", 3:"typed_punct_entity_front", 4: "entity_mask", 5: "entity_marker", 6:"typed_entity_marker"} 
+        model_type = {0: "base-model", 1: "Masked_QA", 2: "MLM Pretraining", 3: "R-BERT"}
+        input_type = {0: "base-input", 1: "typed_punct_entity", 2: "Masked_QA", 3:"typed_punct_entity_front", 4: "entity_mask", 5: "entity_marker", 6:"typed_entity_marker"}
         loss_type = {0: CrossEntropy, 1: Weighted_CrossEntropy, 2: FocalLoss}
         '''
         self.train_type: int = 0
-        self.model_type: int = 1
+        self.model_type: int = 3
         self.input_type: int = 1
         self.loss_type: int = 2
         self.pooling = "MEAN" # 어떤 pooler output을 사용할 것인지 : ["MEAN", "CLS"]
-        self.epoch: int = 3
-        self.checkpoint_dir: str = "./results/QA/roberta-large" # Trainer의 학습 checkpoint 저장 경로.
-        self.label_dict_dir: str = "./results/QA/roberta-large/label2num.pickle" # RESCENT : label2num dictionary save path.
+        self.epoch: int = 2
+        self.checkpoint_dir: str = "./results/rbert" # Trainer의 학습 checkpoint 저장 경로.
+        self.label_dict_dir: str = "./results/rbert/label2num.pickle" # RESCENT : label2num dictionary save path.
         self.warmup_step: int = 500 # learning rate warmup step.
         self.eval_step: int = 500 # 모델 평가/저장 step 수.
+        self.entity_from: str = "last" # ["last", "middle"]
 
         ## 4. 모델 하이퍼파라미터
         self.num_hidden_layer: int = 0 # BERT 뒤에 linear layer를 몇 개 쌓을지.
