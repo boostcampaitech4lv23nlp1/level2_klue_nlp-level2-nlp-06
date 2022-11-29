17c5898a41eb42db800b4a0015e6e79f9b0c3998
JunHwi <random_5@naver.com>
2022-11-29 08:27:19 +0000

    fix: few changes

--- main/model/models.py
+++ main/model/models.py
@@ -98,7 +98,7 @@ class TransformerModelUsingMask(nn.Module):
             self.lstm = nn.LSTM(self.h_dim, self.h_dim, num_layers=1, bias=True, batch_first=True, dropout=0.1, bidirectional=False)
             layers.append(nn.Linear(self.h_dim, self.config.num_labels))
         elif self.rnn_type == "gru":
-            self.gru = nn.GRU(self.h_dim, self.h_dim, num_layers=2, bias=True, batch_first=True, dropout=0.1, bidirectional=True)
+            self.gru = nn.GRU(self.h_dim, self.h_dim, num_layers=1, bias=True, batch_first=True, dropout=0.1, bidirectional=True)
             layers.append(nn.Linear(self.h_dim*2, self.config.num_labels))
         else:
             layers.append(nn.Linear(self.h_dim, self.config.num_labels))
--- main/run.py
+++ main/run.py
@@ -5,31 +5,31 @@ class Config:
     def __init__(self):
         ## 1. 모델 학습시킬 때 가장 먼저 바꿔야 하는 것
         self.model_name: str = "klue/roberta-large"
-        self.save_path: str = "../../saved_model/tt.pt" # 최종 모델을 어디에 저장할지
+        self.save_path: str = "../../saved_model/test.pt" # 최종 모델을 어디에 저장할지
         self.result_path: str = "../../dataset/submission.csv" # 마지막 csv 파일을 어디에 저장할지
-        self.wandb_name: str = "'add lstm test4'" # wandb 내에서 작업 이름 설정 (중요)
-        self.wandb_group: str = "'add rnn test'" # wandb group.
-        self.wandb_note: str = "'lstm, num_layers=1, bias=True, dropout=0.1, bidirectional=False'" # wandb note
+        self.wandb_name: str = "'gru test'" # wandb 내에서 작업 이름 설정 (중요)
+        self.wandb_group: str = "'add rnn test" # wandb group.
+        self.wandb_note: str = "'gru, num_layers=1, bias=True, batch_first=True, dropout=0.1, bidirectional=True'" # wandb note
         
         ## 2. 데이터 위치 (csv 파일)
         self.train_data_path: str = "../../dataset/train/train8/total_train.csv"
-        self.val_data_path: str = "../../dataset/valid/valid8/total_valid.csv"
+        self.val_data_path: str = "../../dataset/train/valid8/total_valid.csv"
         self.test_data_path: str = "../../dataset/test/test_data.csv"
 
         ## 3. 학습 설정
         '''
         train_type = {0: "base-model", 1: "rescent"}
-        model_type = {0: "base-model", 1: "Masked_QA"}
-        input_type = {0: "base-input", 1: "typed_punct_entity", 2: "Masked_QA", 3:"typed_punct_entity_front", 4: "entity_mask", 5: "entity_marker", 6:"typed_entity_marker"} 
+        model_type = {0: "base-model", 1: "Masked_QA", 2: "MLM", 3: "R-BERT"}
+        input_type = {0: "base-input", 1: "typed_punct_entity", 2: "Masked_QA", 3: "typed_punct_entity_front", 4: "entity_mask", 5: "entity_marker", 6: "typed_entity_marker"}
         
         '''
         self.train_type: int = 0
         self.model_type: int = 1
         self.input_type: int = 1
         self.pooling = "MEAN" # 어떤 pooler output을 사용할 것인지 : ["MEAN", "CLS"]
-        self.rnn_type: str = 'lstm' #['lstm', 'gru', None(사용 안함)] - rnn_type 사용 시 pooling은 적용되지 않음. 참고.
-        self.epoch: int = 3
-        self.checkpoint_dir: str = "./results/klue/roberta-large/add_rnn_test/test5" # Trainer의 학습 checkpoint 저장 경로.
+        self.rnn_type = "gru"
+        self.epoch: int = 5
+        self.checkpoint_dir: str = "./results/klue/roberta-large/add_rnn_test/test6" # Trainer의 학습 checkpoint 저장 경로.
         self.label_dict_dir: str = None # RESCENT : label2num dictionary save path.
         self.warmup_step: int = 500 # learning rate warmup step.
         self.eval_step: int = 500 # 모델 평가/저장 step 수.
