163b6286fc6e5db9ad4e3beef20f02c9118823c7
ymnseol <ymnseol@gmail.com>
2022-11-29 12:57:21 +0000

    feat: Add the model selecting state for R-BERT

--- main/model/model_selection.py
+++ main/model/model_selection.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 from argparse import Namespace
 from transformers import AutoTokenizer, AutoModel, AutoConfig, AutoModelForSequenceClassification, AutoModelForMaskedLM
-from model.models import TransformerModel, TransformerModelUsingMask
+from model.models import TransformerModel, TransformerModelUsingMask, TransformerModelUsingEntity
 
 
 class Selection():
@@ -57,6 +57,18 @@ class Selection():
                 self.config.model_name,
                 config=model_config
             )
+        ## R-BERT
+        elif self.config.model_type == 3:
+            print('###', self.config.entity_from)
+            model_config = AutoConfig.from_pretrained(self.config.model_name)
+            model_config.num_labels = 30
+            if self.config.entity_from == "middle":
+                model_config.output_hidden_states = True
+            
+            transformer = AutoModel.from_pretrained(self.config.model_name, config=model_config, add_pooling_layer=False)
+
+            self.model = TransformerModelUsingEntity(transformer, config=model_config, entity_from=self.config.entity_from)
+            self.model.config = model_config
 
         ## TODO: 다른 모델을 사용할 경우
         '''
